5ae6e5afedec80d28cc47d2a17192d45ca78312d
Andreas Lorensen <andlo@outlook.dk>
2018-11-03 09:19:19 +0000

    testXy

--- __init__.py
+++ __init__.py
@@ -12,12 +12,12 @@ class Fairytalez(MycroftSkill):
     def handle_fairytalez(self, message):
         #self.speak_dialog('fairytalez')
         self.is_reading = True
-        lines = get_story(self) 
+        lines = self.get_story() 
         for line in lines:
             if self.is_reading == False:
                 break
-            wait_while_speaking()
-            self.speak(line)
+            #wait_while_speaking()
+            self.speak(line, wait=True)
         self.is_reading = False
 
     def stop(self):
@@ -28,18 +28,31 @@ class Fairytalez(MycroftSkill):
             return BeautifulSoup(requests.get(url).text,"html.parser")
         except Exception as SockException:
             print(SockException)
-            sys.exit(1)
+            #sys.exit(1)
     
-    def get_story(self):
-        self.url = "https://fairytalez.com/the-little-mermaid/"
-        self.url+=type+".html"
-        soup = get_soup(self.url)
-        lines = "".join([i.text for i in soup.select("#main > article > section > p")])
+#    def get_story(self):
+#        url = "https://fairytalez.com/the-little-mermaid/"
+#        #self.url+=type+".html"
+#        #soup = self.get_soup(self.url)
+#        soup = BeautifulSoup(requests.get(url).text,"html.parser")
+#        #/lines = "".join([i.text for i in soup.select("#main > article > section > p")])
+##        section = soup.select("#main > article > section")
+#        lines = section.find_all("p") 
+#        return lines
+    
+    def get_story():
+        url = "https://fairytalez.com/the-little-mermaid/"
+        #soup = get_soup(url)
+        soup = BeautifulSoup(requests.get(url).text,"html.parser")
+        # ignore first entry, its just garbage
+        # Already a member? Sign in. Or Create a free Fairytalez account in less than a minute.
+        lines = [a.text.strip() for a in soup.find(id="main").find_all("p")[1:]]
+        # you might want to also remove {Note: You can read an illustrated version of this story, plus other mermaid tales, in our collection Mermaid Tales: The Little Mermaid and 14 Other Illustrated Mermaid Stories, now available for Amazon Kindle.}
+        # i just checked if line starts and ends with { , this may depend on the
+        #  story so using the index is bad idea, but filtering everything
+        # betwene {} should be safe
+        lines = [l for l in lines if not l.startswith("{") and not l.endswith("}")]
         return lines
-    
-
-
-    
 
 def create_skill():
     return Fairytalez()
