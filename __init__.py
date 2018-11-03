ff1b935ea687cfee40010765ab1feb5b2bb1ee51
Andreas Lorensen <andlo@outlook.dk>
2018-11-03 19:29:02 +0000

    test

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft.util.parse import match_one
+
 import requests
 from bs4 import BeautifulSoup
 import sys
@@ -10,9 +12,19 @@ class Fairytalez(MycroftSkill):
 
     @intent_file_handler('fairytalez.intent')
     def handle_fairytalez(self, message):
-        #self.speak_dialog('fairytalez')
-        self.tell_story("https://fairytalez.com/the-little-mermaid/")
+        response = self.get_response('fairytalez')
+        self.speak_dialog('let_me_think', data={"story":response})
+        index = self.get_index("https://fairytalez.com/fairy-tales/")
+        result = match_one(response, list(index.keys()))
+        if result[1] < 0.8:
+            self.speak_dialog('that_would_be', data={"story":result[0]})
+        else:
+            self.speak_dialog('i_know_that', data={"story":result[0]})
         
+        self.log.info(result)
+        self.log.info(index.get(result[0]))
+        self.tell_story(index.get(result[0]))
+
     def tell_story(self, url):
         self.is_reading = True
         lines = self.get_story(url) 
@@ -45,9 +57,10 @@ class Fairytalez(MycroftSkill):
 
     def get_index(self, url):
         soup = BeautifulSoup(requests.get(url).text,"html.parser")
-        index = []
+        index = {}
         for link in soup.find(id="main").find_all('a'):
-            index.append([link.text[2:], link.get("href")])
+            #index.append([link.text[2:], link.get("href")])
+            index.update({link.text[2:] : link.get("href")})
         return index
 
 def create_skill():
