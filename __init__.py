a7f2ab5c1aff75454d239c74c84112a9e243dcf0
andlo <andlo@outlook.dk>
2018-12-10 19:16:35 +0000

    added author and info about fairytalez.com

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from mycroft import MycroftSkill, intent_file_handler
 from mycroft.util.parse import match_one
 import requests
 from bs4 import BeautifulSoup
+import time
 
 
 class Fairytalez(MycroftSkill):
@@ -20,13 +21,14 @@ class Fairytalez(MycroftSkill):
 
         if result[1] < 0.8:
             self.speak_dialog('that_would_be', data={"story": result[0]})
-            response = self.ask_yesno('is_it')
+            response = self.ask_yesno('is_it_that')
             if not response:
                 self.speak_dialog('no_story')
                 return
         self.speak_dialog('i_know_that', data={"story": result[0]})
         # self.log.info(result + " " + result[0])
         self.settings['story'] = result[0]
+        time.sleep(3)
         self.tell_story(index.get(result[0]), 0)
 
     @intent_file_handler('continue.intent')
@@ -42,15 +44,22 @@ class Fairytalez(MycroftSkill):
     def tell_story(self, url, bookmark):
         self.is_reading = True
         self.settings['bookmark'] = bookmark
+        title = self.get_title(url)
+        author = self.get_author(url)
+        self.speak_dialog('title_by_author', data={'title': title, 'author': author})
+        time.sleep(2)
         lines = self.get_story(url)
         for line in lines[bookmark:]:
             if not self.is_reading:
                 return
             self.speak(line, wait=True)
             self.settings['bookmark'] += 1
+            time.sleep(1)
         self.is_reading = False
         self.settings['bookmark'] = 0
         self.settings['story'] = None
+        time.sleep(5)
+        self.speak_dialog('from_fairytalez')
 
     def stop(self):
         if self.is_reading is True:
@@ -68,6 +77,16 @@ class Fairytalez(MycroftSkill):
         lines = [a.text.strip() for a in soup.find(id="main").find_all("p")[1:]]
         lines = [l for l in lines if not l.startswith("{") and not l.endswith("}")]
         return lines
+
+    def get_title(self, url):
+        soup = self.get_soup(url)
+        title = [a.text.strip() for a in soup.findAll("h1")][0]
+        return title
+
+    def get_author(self, url):
+        soup = self.get_soup(url)
+        author = [a.text.strip() for a in soup.findAll("div", {"class": "author-name"})][0]
+        return str(author).split("  ")[0]
 
     def get_index(self, url):
         soup = self.get_soup(url)
